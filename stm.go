99b0efdf7c95254e9b78dc8b8d0666cea508c510
eihigh <eihigh.contact@gmail.com>
2018-11-16 17:54:01 +0900

    add Degree method

--- stm.go
+++ stm.go
@@ -21,6 +21,11 @@ func (s Stm) HasElapsed(frames int) bool {
 	return s.count > frames
 }
 
+// Degree returns how much it reached of the goal
+func (s Stm) Degree(goal int) float64 {
+	return float64(s.count) / float64(goal)
+}
+
 // Reset resets the count
 func (s *Stm) Reset() {
 	s.count = 0
